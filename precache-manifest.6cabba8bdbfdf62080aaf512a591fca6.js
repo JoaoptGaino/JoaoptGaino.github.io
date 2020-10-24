f46825731fd5d258d66a7393ee58de4b4d83f137
joaoptgaino <joaoptgaino@hotmail.com>
2020-10-24 18:38:05 -0300

    Updates

---
+++ precache-manifest.6cabba8bdbfdf62080aaf512a591fca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7aee1fd67b875fe19ffed71ce8b6ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03d4bb0ad9269c434567",
+    "url": "./static/css/main.b4a39a32.chunk.css"
+  },
+  {
+    "revision": "ed7000cae35ac89cf659",
+    "url": "./static/js/2.dab409c7.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "./static/js/2.dab409c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d4bb0ad9269c434567",
+    "url": "./static/js/main.a7ca2607.chunk.js"
+  },
+  {
+    "revision": "ef50225270117dc78a7c",
+    "url": "./static/js/runtime-main.50165525.js"
+  },
+  {
+    "revision": "ce3bb07e3e5dd7a558090f41f2ca86f3",
+    "url": "./static/media/eu.ce3bb07e.jpg"
+  }
+]);
